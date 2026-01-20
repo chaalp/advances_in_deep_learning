ecd61e0260198bde63939e0e286b350baa8869b9
chaalp <chander_alphonse@yahoo.com>
2026-01-20 10:52:36 -0600

    Initial commit

--- homework1/homework/lower_precision.py
+++ homework1/homework/lower_precision.py
@@ -1,8 +1,14 @@
 from pathlib import Path
+import torch
 
+from .low_precision import BigNet4Bit  # reuse 4-bit model
 
 def load(path: Path | None):
     # TODO (extra credit): Implement a BigNet that uses in
     # average less than 4 bits per parameter (<9MB)
     # Make sure the network retains some decent accuracy
-    return None
+    #return None
+    net = BigNet4Bit()
+    if path is not None:
+        net.load_state_dict(torch.load(path, weights_only=True), strict=False)
+    return net
