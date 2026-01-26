c77e71e5b1b083f1212db70a2814c6ae31d3b2a2
chaalp <chander_alphonse@yahoo.com>
2026-01-26 15:26:35 -0600

    local

--- homework3/homework/base_llm.py
+++ homework3/homework/base_llm.py
@@ -129,7 +129,7 @@ class BaseLLM:
 
         # Generation params
         gen_kwargs = dict(
-            max_new_tokens=50,
+            max_new_tokens=24,
             min_new_tokens=1,
             eos_token_id=self.tokenizer.eos_token_id,
             pad_token_id=self.tokenizer.eos_token_id,
