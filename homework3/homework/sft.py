db54865e74e74a3c1845052767d6c087d809adc3
chaalp <chander_alphonse@yahoo.com>
2026-01-26 10:27:10 -0600

    local

--- homework3/homework/sft.py
+++ homework3/homework/sft.py
@@ -167,6 +167,8 @@ def train_model(
         save_total_limit=2,
         remove_unused_columns=False,  # important since our dataset returns dicts
         label_names=["labels"], # Common default
+        fp16=False,
+        bf16=False,
     )
 
     trainer = Trainer(
