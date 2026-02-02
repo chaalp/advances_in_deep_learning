68dd325da7e00e19ea5a7de5359d6d1f0f2529bd
chaalp <chander_alphonse@yahoo.com>
2026-02-02 08:24:52 -0600

    local

--- homework3/homework/rft.py
+++ homework3/homework/rft.py
@@ -54,8 +54,8 @@ def train_model(
 
     # LoRA Config - The README suggests a slightly larger adapter for RFT
     config = LoraConfig(
-        r=8,
-        lora_alpha=32, 
+        r=16,
+        lora_alpha=80, 
         target_modules="all-linear", 
         bias="none", 
         task_type="CAUSAL_LM"
