a8a814ab879048487ade1ea830e12ea27858f115
chaalp <chander_alphonse@yahoo.com>
2026-02-04 12:25:10 -0600

    local

--- homework3/homework/base_llm.py
+++ homework3/homework/base_llm.py
@@ -134,7 +134,7 @@ class BaseLLM:
         # Call model.generate
         output_ids = self.model.generate(
             **inputs,
-            max_new_tokens=48,
+            max_new_tokens=max_tokens,
             do_sample=temperature > 0,
             temperature=temperature if temperature > 0 else 1.0,
             num_return_sequences=num_return_sequences or 1,
