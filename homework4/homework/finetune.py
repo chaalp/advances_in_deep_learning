208bae263f5abffb67665a32746aa9c8be36a4ca
chaalp <chander_alphonse@yahoo.com>
2026-02-11 07:19:38 -0600

    local

--- homework4/homework/finetune.py
+++ homework4/homework/finetune.py
@@ -112,11 +112,11 @@ def train(
     train_dataset_name: str = "train",
     output_dir: str = "vlm_sft",
     num_train_epochs: int = 0.05,  # use only 0.05 epoch for training
-    per_device_train_batch_size: int = 8,
-    gradient_accumulation_steps: int = 4,
+    per_device_train_batch_size: int = 16,
+    gradient_accumulation_steps: int = 8,
     learning_rate: float = 5e-4,
-    lora_r: int = 16,
-    lora_alpha: int = 64,
+    lora_r: int = 8,
+    lora_alpha: int = 32,
     lora_dropout: float = 0.0,
     num_workers: int = 16,
 ):
