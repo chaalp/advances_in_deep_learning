19f9e1fbed64fcba88bb63fcb240c875a9b981eb
chaalp <chander_alphonse@yahoo.com>
2026-02-13 11:36:40 -0600

    local

--- homework4/homework/clip.py
+++ homework4/homework/clip.py
@@ -317,8 +317,8 @@ def get_target_modules_for_lora(model: nn.Module) -> list[str]:
 def train(
     data_dir: Path | None = None,
     output_dir: str = "clip_model",
-    num_train_epochs: float = 0.2,  # for debugging purpose, increase this once the dry run works
-    per_device_train_batch_size: int = 9,
+    num_train_epochs: float = 0.1,  # for debugging purpose, increase this once the dry run works
+    per_device_train_batch_size: int = 4,
     gradient_accumulation_steps: int = 1,
     learning_rate: float = 5e-4,
     num_workers: int = 2,
