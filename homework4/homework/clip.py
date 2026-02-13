1653fb116ff45b6eeb5e9f4c4c17ff408b1f84a3
chaalp <chander_alphonse@yahoo.com>
2026-02-13 11:37:45 -0600

    local

--- homework4/homework/clip.py
+++ homework4/homework/clip.py
@@ -318,7 +318,7 @@ def train(
     data_dir: Path | None = None,
     output_dir: str = "clip_model",
     num_train_epochs: float = 0.1,  # for debugging purpose, increase this once the dry run works
-    per_device_train_batch_size: int = 4,
+    per_device_train_batch_size: int = 2,
     gradient_accumulation_steps: int = 1,
     learning_rate: float = 5e-4,
     num_workers: int = 2,
