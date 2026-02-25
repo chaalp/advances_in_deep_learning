5c97b55325fc247f30429eee77bbfa6d058722c6
chaalp <chander_alphonse@yahoo.com>
2026-02-25 12:21:16 -0600

    local

--- homework4/homework/finetune.py
+++ homework4/homework/finetune.py
@@ -131,7 +131,7 @@ def train(
     gradient_accumulation_steps: int = 4,
     learning_rate: float = 5e-4,
     lora_r: int = 16,
-    lora_alpha: int = 32,
+    lora_alpha: int = 64,
     lora_dropout: float = 0.0,
     num_workers: int = 2,
 ):
